321bb8f17f5c1ef02479597236f7c3482164cef2
Srijan Das <srijand1531@gmail.com>
2026-04-04 21:35:03 +0530

    fix: enable CORS to resolve API access issue

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const Book=require("./book.models")
 
 app.use(express.json())
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 initializeDataBase()
+
+
 
 app.get("/",(req,res)=>{
     res.send("Hello!")
